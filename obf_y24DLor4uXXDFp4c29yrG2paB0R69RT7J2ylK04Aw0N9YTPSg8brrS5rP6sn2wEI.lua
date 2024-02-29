937d5dc67302b863ecdda539960f7db3ef7bef2c
luongkhang1510 <161694218+luongkhang1510@users.noreply.github.com>
2024-02-29 14:48:39 +0700

    Add files via upload

---
+++ obf_y24DLor4uXXDFp4c29yrG2paB0R69RT7J2ylK04Aw0N9YTPSg8brrS5rP6sn2wEI.lua
@@ -0,0 +1,5 @@
+---// Join MTien Hub Sever To Update New Script \\--
+--// Sever Discord: https://discord.com/invite/pCBDH5z382 \\--
+_G.Name = "khangthecat" -- Put You Name Script Here
+_G.Logo = "file:///storage/emulated/0/Download/portada_hatsune-miku-35.jpg" -- Enter Your ID Image Here
+loadstring(Game:HttpGet"https://raw.githubusercontent.com/ImTienNguyenZ/NguyenTienSimpRose/main/NguyenMinhTienLoader")()
